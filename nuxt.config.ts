93c05c6b46e1943ad686e2908d7e8de7724ae3cd
kota_shiratori <swan.kouta@gmail.com>
2024-04-01 02:16:22 +0900

    🎨 Github Pages用

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    "@pinia/nuxt",
-  ],
+  modules: ["@pinia/nuxt"],
+  app: {
+    baseURL: "/nuxt3-pinia-todo/",
+  },
 });
